06562fe9d1919f63fce7eb28a7240ae7cf3a254f
Umesh <dhangareumesh@gmail.com>
2023-09-16 00:06:31 +0530

    Cors Allowed all origions

--- Backend_NodeJS/app.js
+++ Backend_NodeJS/app.js
@@ -30,7 +30,7 @@ app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3001" }));
+app.use(cors({ origin: "*" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
